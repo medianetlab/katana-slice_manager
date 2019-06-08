ec094c2e16dd01cc5b3bec76581d10121cd18bf7
themis <thmanagnostopoulos@iit.demokritos.gr>
2019-06-08 16:08:57 +0300

    install.sh checks for installed application of katana

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Install the command for the cli tool to /usr/local/bin/
-mv katana /usr/local/bin/
+command -v katana || mv katana /usr/local/bin/
 
 # Build the images
 docker-compose -f katana-mngr/docker-compose.yaml build
