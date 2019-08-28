534fb372eb2d1ed03f057fd18d9d6fa4fc1322ab
Anastasios Gogos <13665570+tgogos@users.noreply.github.com>
2019-08-28 18:40:35 +0300

    add proxy handler for listing VIMs

--- katana-ui/ui/api/mngr/api_endpoints.py
+++ katana-ui/ui/api/mngr/api_endpoints.py
@@ -17,7 +17,24 @@ class ApiView(MngrFlaskView):
 
     # code from: https://stackoverflow.com/questions/6656363/proxying-to-another-web-service-with-flask
 
-    @route('/vim')
+    @route('/vim', endpoint='vim_list')
+    @jwt_required
+    def vim_list(self):
+        resp = requests.request(
+            method=request.method,
+            url=request.url.replace(request.host_url, 'http://katana-mngr:8000/'),
+            headers={key: value for (key, value) in request.headers if key != 'Host'},
+            data=request.get_data(),
+            cookies=request.cookies,
+            allow_redirects=False)
+
+        excluded_headers = ['content-encoding', 'content-length', 'transfer-encoding', 'connection']
+        headers = [(name, value) for (name, value) in resp.raw.headers.items()
+                   if name.lower() not in excluded_headers]
+
+        response = Response(resp.content, resp.status_code, headers)
+        return response
+
     @route('/vim/<id>', endpoint='vim_handle')
     @jwt_required
     def vim_handle(self, id):
