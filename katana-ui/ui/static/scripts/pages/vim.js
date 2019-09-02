dc024233e64941abea786b697084df6153f714e8
Anastasios Gogos <13665570+tgogos@users.noreply.github.com>
2019-09-02 14:50:59 +0300

    add toastr js lib for popup messages

--- katana-ui/ui/static/scripts/pages/vim.js
+++ katana-ui/ui/static/scripts/pages/vim.js
@@ -1,8 +1,21 @@
 $(document).ready(function(){
-	var source   = $("#vim-table").html();
-	var template = Handlebars.compile(source);
 
-	$.ajax({
+    // global settings for toastr js (plugin for popup messages)
+    toastr.options.closeButton = true;
+    toastr.options.progressBar = true;
+
+    render_vim_table();
+
+});
+
+
+function render_vim_table() {
+    // template for vim-table
+    var source   = $("#vim-table").html();
+    var template = Handlebars.compile(source);
+
+    // try to fetch the json with vim data
+    $.ajax({
         type: 'GET',
         url: '/api/vim/all',
         timeout: 15000,
@@ -10,6 +23,7 @@ $(document).ready(function(){
         error: function(err)
         {
             console.log(err);
+            toastr.error(err.status + " " + err.statusText,"GET /api/vim/all error");
         },
         dataType: 'json',
         success: function(data) {
@@ -17,6 +31,38 @@ $(document).ready(function(){
             var html    = template(data);
             $('.vim-table-tpl').html(html);
 
+            // if the template is rendered and the table is added to the page
+            // add listeners
+            add_vim_button_group_listeners();
+
         }
     });
-});
+}
+
+
+function add_vim_button_group_listeners() {
+    $('.btn-rm').on('click', function(){
+       rm_vim(($(this).parent().attr('data-uuid')))        
+    });
+}
+
+
+function rm_vim(uuid) {
+    $.ajax({
+        type: 'DELETE',
+        url: '/api/vim/'+uuid,
+        timeout: 15000,
+
+        error: function(err)
+        {
+            console.log(err);
+            toastr.error(err.status + " " + err.statusText,"DELETE /api/vim/<uuid> error");
+        },
+        dataType: 'json',
+        success: function(data) {
+            console.log(data);
+            toastr.success("Vim has been removed successfully");
+            $('.vim-trow-'+uuid).remove();
+        }
+    });
+}
