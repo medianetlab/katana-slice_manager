14dc69de95c6a594c5715aea57cbcc684a849d24
Anastasios Gogos <13665570+tgogos@users.noreply.github.com>
2019-09-12 14:03:46 +0300

    remove obsolete code, add description to proxy function

--- katana-ui/ui/api/mngr/vim_view.py
+++ katana-ui/ui/api/mngr/vim_view.py
@@ -11,98 +11,35 @@ class VimView(MngrFlaskView):
 
     trailing_slash = False
 
-    # code from: https://stackoverflow.com/questions/6656363/proxying-to-another-web-service-with-flask
-
     @jwt_required
     def index(self):
-        # resp = requests.request(
-        #     method=request.method,
-        #     url=request.url.replace(request.host_url, 'http://katana-mngr:8000/').replace('/mngr/api','/api'),
-        #     headers={key: value for (key, value) in request.headers if key != 'Host'},
-        #     data=request.get_data(),
-        #     cookies=request.cookies,
-        #     allow_redirects=False)
-
-        # excluded_headers = ['content-encoding', 'content-length', 'transfer-encoding', 'connection']
-        # headers = [(name, value) for (name, value) in resp.raw.headers.items()
-        #            if name.lower() not in excluded_headers]
-
-        # response = Response(resp.content, resp.status_code, headers)
-        # return response
         return proxy_request_to_katana_mngr(request)
 
     @jwt_required
     def get(self, id):
-        # resp = requests.request(
-        #     method=request.method,
-        #     url=request.url.replace(request.host_url, 'http://katana-mngr:8000/').replace('/mngr/api','/api'),
-        #     headers={key: value for (key, value) in request.headers if key != 'Host'},
-        #     data=request.get_data(),
-        #     cookies=request.cookies,
-        #     allow_redirects=False)
-
-        # excluded_headers = ['content-encoding', 'content-length', 'transfer-encoding', 'connection']
-        # headers = [(name, value) for (name, value) in resp.raw.headers.items()
-        #            if name.lower() not in excluded_headers]
-
-        # response = Response(resp.content, resp.status_code, headers)
-        # return response
         return proxy_request_to_katana_mngr(request)
 
     @jwt_required
     def delete(self, uuid):
-        # resp = requests.request(
-        #     method=request.method,
-        #     url=request.url.replace(request.host_url, 'http://katana-mngr:8000/').replace('/mngr/api','/api'),
-        #     headers={key: value for (key, value) in request.headers if key != 'Host'},
-        #     data=request.get_data(),
-        #     cookies=request.cookies,
-        #     allow_redirects=False)
-
-        # excluded_headers = ['content-encoding', 'content-length', 'transfer-encoding', 'connection']
-        # headers = [(name, value) for (name, value) in resp.raw.headers.items()
-        #            if name.lower() not in excluded_headers]
-
-        # response = Response(resp.content, resp.status_code, headers)
-        # return response
         return proxy_request_to_katana_mngr(request)
 
     @jwt_required
     def put(self, uuid):
-        # resp = requests.request(
-        #     method=request.method,
-        #     url=request.url.replace(request.host_url, 'http://katana-mngr:8000/').replace('/mngr/api','/api'),
-        #     headers={key: value for (key, value) in request.headers if key != 'Host'},
-        #     data=request.get_data(),
-        #     cookies=request.cookies,
-        #     allow_redirects=False)
-
-        # excluded_headers = ['content-encoding', 'content-length', 'transfer-encoding', 'connection']
-        # headers = [(name, value) for (name, value) in resp.raw.headers.items()
-        #            if name.lower() not in excluded_headers]
-
-        # response = Response(resp.content, resp.status_code, headers)
-        # return response
         return proxy_request_to_katana_mngr(request)
 
     @jwt_required
     def post(self):
-        # resp = requests.request(
-        #     method=request.method,
-        #     url=request.url.replace(request.host_url, 'http://katana-mngr:8000/').replace('/mngr/api','/api'),
-        #     headers={key: value for (key, value) in request.headers if key != 'Host'},
-        #     data=request.get_data(),
-        #     cookies=request.cookies,
-        #     allow_redirects=False)
-
-        # excluded_headers = ['content-encoding', 'content-length', 'transfer-encoding', 'connection']
-        # headers = [(name, value) for (name, value) in resp.raw.headers.items()
-        #            if name.lower() not in excluded_headers]
-
-        # response = Response(resp.content, resp.status_code, headers)
-        # return response
         return proxy_request_to_katana_mngr(request)
 
+
+
+"""
+Until commit df65c9c0ec5ae7930d5145bd9eac947da1f269c4
+the code below was inside each function (index / get / delete / put / post)
+To reduce repetition of code, we introduce the "proxy_request_to_katana_mngr()" function below.
+
+code from: https://stackoverflow.com/questions/6656363/proxying-to-another-web-service-with-flask
+"""
 def proxy_request_to_katana_mngr(request):
     resp = requests.request(
         method=request.method,
