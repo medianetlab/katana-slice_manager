e87a3b025b3c5da5a9561dfaef07f887c0106a13
Anastasios Gogos <13665570+tgogos@users.noreply.github.com>
2019-10-01 13:03:24 +0300

    add --volumes to remove volumes without specifying their names

--- clear.sh
+++ clear.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
 # Stop the containers
-docker-compose -f docker-compose-ui.yaml down
-docker volume rm slicemanager_mongo-configdb slicemanager_mongo-datadb slicemanager_postgres
+docker-compose down --volumes
