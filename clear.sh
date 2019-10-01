55376dee58c1740e9fda2c0d1c894284aa038355
themis <thmanagnostopoulos@gmail.com>
2019-10-01 18:36:21 +0300

    Added to clear.sh to remove log flies

--- clear.sh
+++ clear.sh
@@ -2,3 +2,14 @@
 
 # Stop the containers
 docker-compose down --volumes
+
+# Remove the katana-container-log file
+file=katana-container-logs
+if [[ -L "$file" ]]; then
+    rm -f $file
+fi
+
+# Remove the katana-log file
+file=katana-mngr/katana.log*
+    rm -f $file
+ 
