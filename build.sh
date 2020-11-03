b1c6b19cb251abf876817d62b55299206998cb4b
themisAnagno <thmanagnostopoulos@gmail.com>
2020-11-03 19:23:32 +0000

    Add push to docker script

--- build.sh
+++ build.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-# Check if the tag number is set
-if [ -z "${TAG_NUMBER+x}" ]; then
-export TAG_NUMBER="test"
-fi
-
 # *** Install development environment ***
 # Check if the --dev option is given
 if [[ "$1" == "--dev" ]];
@@ -27,24 +22,53 @@ then
 
 fi
 
+# Check if the user and release fare defined
+while [[ $# -gt 0 ]]
+do
+    key=$1
+
+    case $key in
+    -r | --release)
+        TAG_NUMBER=$2
+        shift
+        shift
+    ;;
+    -u | --user)
+        DOCKER_USER=$2
+        shift
+        shift
+    ;;
+    esac
+done
+
+# Check if the tag number is set
+if [ -z "${TAG_NUMBER+x}" ]; then
+export TAG_NUMBER="test"
+fi
+
+# Check if the DOCKER_USER is set
+if [ -z "${DOCKER_USER+x}" ]; then
+export DOCKER_USER="mnlab"
+fi
+
 # Install the command for the cli tool to /usr/local/bin/
 command -v katana &> /dev/null || cp katana /usr/local/bin/
 
 # Build the images
 echo "**** Building the images ****"
-docker image build -f  katana-nbi/Dockerfile -t mnlab/katana-nbi:${TAG_NUMBER} .
-docker image build -f  katana-mngr/Dockerfile -t mnlab/katana-mngr:${TAG_NUMBER} .
-docker image build -f  katana-cli/Dockerfile -t mnlab/katana-cli:${TAG_NUMBER} .
-docker image build -f  katana-swagger/Dockerfile -t mnlab/katana-swagger:${TAG_NUMBER} .
-docker image build -f  katana-prometheus/Dockerfile -t mnlab/katana-prometheus:${TAG_NUMBER} .
-docker image build -f  katana-grafana/Dockerfile -t mnlab/katana-grafana:${TAG_NUMBER} .
-docker image build -f  katana-nfv_mon/Dockerfile -t mnlab/katana-nfv_mon:${TAG_NUMBER} .
+docker image build -f katana-nbi/Dockerfile -t "${DOCKER_USER}"/katana-nbi:${TAG_NUMBER} .
+docker image build -f katana-mngr/Dockerfile -t "${DOCKER_USER}"/katana-mngr:${TAG_NUMBER} .
+docker image build -f katana-cli/Dockerfile -t "${DOCKER_USER}"/katana-cli:${TAG_NUMBER} .
+docker image build -f katana-swagger/Dockerfile -t "${DOCKER_USER}"/katana-swagger:${TAG_NUMBER} .
+docker image build -f katana-prometheus/Dockerfile -t "${DOCKER_USER}"/katana-prometheus:${TAG_NUMBER} .
+docker image build -f katana-grafana/Dockerfile -t "${DOCKER_USER}"/katana-grafana:${TAG_NUMBER} .
+docker image build -f katana-nfv_mon/Dockerfile -t "${DOCKER_USER}"/katana-nfv_mon:${TAG_NUMBER} .
 
 # Tag the latest images
-docker image tag mnlab/katana-nbi:${TAG_NUMBER} mnlab/katana-nbi:latest
-docker image tag mnlab/katana-mngr:${TAG_NUMBER} mnlab/katana-mngr:latest
-docker image tag mnlab/katana-cli:${TAG_NUMBER} mnlab/katana-cli:latest
-docker image tag mnlab/katana-swagger:${TAG_NUMBER} mnlab/katana-swagger:latest
-docker image tag mnlab/katana-prometheus:${TAG_NUMBER} mnlab/katana-prometheus:latest
-docker image tag mnlab/katana-grafana:${TAG_NUMBER} mnlab/katana-grafana:latest
-docker image tag mnlab/katana-nfv_mon:${TAG_NUMBER} mnlab/katana-nfv_mon:latest
+docker image tag "${DOCKER_USER}"/katana-nbi:${TAG_NUMBER} "${DOCKER_USER}"/katana-nbi:latest
+docker image tag "${DOCKER_USER}"/katana-mngr:${TAG_NUMBER} "${DOCKER_USER}"/katana-mngr:latest
+docker image tag "${DOCKER_USER}"/katana-cli:${TAG_NUMBER} "${DOCKER_USER}"/katana-cli:latest
+docker image tag "${DOCKER_USER}"/katana-swagger:${TAG_NUMBER} "${DOCKER_USER}"/katana-swagger:latest
+docker image tag "${DOCKER_USER}"/katana-prometheus:${TAG_NUMBER} "${DOCKER_USER}"/katana-prometheus:latest
+docker image tag "${DOCKER_USER}"/katana-grafana:${TAG_NUMBER} "${DOCKER_USER}"/katana-grafana:latest
+docker image tag "${DOCKER_USER}"/katana-nfv_mon:${TAG_NUMBER} "${DOCKER_USER}"/katana-nfv_mon:latest
---
+++ push_docker_image.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+# Push docker images to docker hub
+
+# *** Docker Login ***
+# Check the options to see if user and password are defined
+while [[ $# -gt 0 ]]
+do
+    key=$1
+
+    case $key in
+    -p | --password)
+        DOCKER_PASSWORD=$2
+        shift
+        shift
+    ;;
+    -u | --user)
+        DOCKER_USER=$2
+        shift
+        shift
+    ;;
+    -r | --release)
+        TAG_NUMBER=$2
+        shift
+        shift
+    ;;
+    esac
+done
+
+# Read the user and password if they are not given as options
+if [ -z "${DOCKER_PASSWORD+x}" ] || [ -z "${DOCKER_USER+x}" ]; then
+read -r -p "Enter the Docker hub user" DOCKER_USER 
+read -r -p -s "Enter the password " DOCKER_PASSWORD
+fi
+
+docker login -u "${DOCKER_USER}" -p "${DOCKER_PASSWORD}"
+
+# Push to the remote Docker Hub
+
+if [ -z "${TAG_RELEASE}" ]; then
+docker image push "${DOCKER_USER}"/katana-nbi:"${TAG_NUMBER}"
+docker image push "${DOCKER_USER}"/katana-mngr:"${TAG_NUMBER}"
+docker image push "${DOCKER_USER}"/katana-cli:"${TAG_NUMBER}"
+docker image push "${DOCKER_USER}"/katana-swagger:"${TAG_NUMBER}"
+docker image push "${DOCKER_USER}"/katana-prometheus:"${TAG_NUMBER}"
+docker image push "${DOCKER_USER}"/katana-grafana:"${TAG_NUMBER}"
+docker image push "${DOCKER_USER}"/katana-nfv_mon:"${TAG_NUMBER}"
+fi
+
+docker image push "${DOCKER_USER}"/katana-nbi:latest
+docker image push "${DOCKER_USER}"/katana-mngr:latest
+docker image push "${DOCKER_USER}"/katana-cli:latest
+docker image push "${DOCKER_USER}"/katana-swagger:latest
+docker image push "${DOCKER_USER}"/katana-prometheus:latest
+docker image push "${DOCKER_USER}"/katana-grafana:latest
+docker image push "${DOCKER_USER}"/katana-nfv_mon:latest
